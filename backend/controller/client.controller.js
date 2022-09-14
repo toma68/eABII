7d8c028cc26e9e9db13190943ee3e41e05948218
Nono <arnaud.851216@gmail.com>
2022-09-14 11:12:14 +0200

    finished client controller + fixes

--- backend/controller/client.controller.js
+++ backend/controller/client.controller.js
@@ -30,25 +30,80 @@ exports.create = (req, res) => {
 
 // Retrieve all Clients from db
 exports.findAll = (req, res) => {
-
+    CLient.findAll()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving Clients."
+            });
+        });
 };
 
 // Find a single Client with an id
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Client.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(400).send({ message: `Cannot find Client with id : ${id}` });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: `Error retrieving Client with id : ${id}` });
+        });
 };
 
 // Update a Client by the id in the request
 exports.update = (req, res) => {
-
+    const id = req.params.id;
+    Tutorial.update(req.body, {
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Tutorial was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Tutorial with id=" + id
+            });
+        });
 };
 
 // Delete a Client with the specified id in the request
 exports.delete = (req, res) => {
+    const id = req.params.id;
 
-};
-
-// Find all published Clients
-exports.findAllPublished = (req, res) => {
-
+    Tutorial.destroy({
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Tutorial was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Tutorial with id=" + id
+            });
+        });
 };
